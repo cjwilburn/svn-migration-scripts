60d668137dff996c4a106e385c63e8783582f6cc
Huw Giddens <hgiddens@gmail.com>
2012-07-18 17:32:06 +1000

    Use “$(…)”.

--- src/main/scala/SyncRebase.scala
+++ src/main/scala/SyncRebase.scala
@@ -4,10 +4,10 @@ object SyncRebase {
   def main(args: Array[String]) {
     "git branch".lines_!.map(_.substring(2)).foreach { branch =>
       val remote = "remotes/" + (if (branch == "master") "trunk" else branch)
-      val lstable = ("git rev-parse --sq heads/" + branch).!!
-      val rstable = ("git rev-parse --sq " + remote).!!
+      val lstable = $("git", "rev-parse", "--sq", "heads/" + branch)
+      val rstable = $("git", "rev-parse", "--sq", remote)
       if (lstable != rstable) {
-        if ("git rebase %s %s".format(remote, branch).! != 0)
+        if (List("git", "rebase", remote, branch).! != 0)
           throw sys.error("error rebasing %s onto %s".format(branch, remote))
       }
     }
