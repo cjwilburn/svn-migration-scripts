ba2f5194a2fe273a3d96785c7edc57649023465d
Pierre-Etienne Poirot <pepoirot@atlassian.com>
2012-07-23 19:23:28 +1000

    Allows to generate the authors for non-OnDemand customers.
    This modifies and renames the command "authors" to "authors-ondemand"
    to allow the generation for both OnDemand and non-OnDemand customers.
    
    For example, the user can run either:
    - "authors https:///mycompany/svn" or
    - "authors-ondemand: instance.jira.com"
    
    Additional changes:
    - the password can now be entered on the prompt instead of specified
      at the command line,
    - the username and password are now optional (this is useful for some
      corner cases like when the user has its svn credentials in the
      keychain)
    - logging was made more consistent with the other commands

--- src/main/scala/Authors.scala
+++ src/main/scala/Authors.scala
@@ -1,15 +1,17 @@
 import dispatch._
 import dispatch.liftjson.Js._
 import io.Source
+import java.lang.String._
 import net.liftweb.json._
+import scala.Array
 import sys.process._
 import xml.pull._
 
 object Authors {
-  val executor = new Http with thread.Safety with NoLogging
+  private val executor = new Http with thread.Safety with NoLogging
 
   // Return a tuple of user name -> email, or None
-  def authorDetails(root: Request, author: String): Option[(String, String)] = {
+  private def authorDetails(root: Request, author: String): Option[(String, String)] = {
     try {
       executor(root / "user?username=%s".format(Request.encode_%(author)) ># { json =>
         (for {
@@ -23,24 +25,24 @@ object Authors {
     }
   }
 
-  def main(args: Array[String]) {
-    if (args.length != 3) {
-      println("args: host username password")
-      sys.exit(1)
-    }
+  def generateList(args: Array[String]) = fetchList(process(args), convertUsers)
+  def generateListForOnDemand(args: Array[String]) = fetchList(processOnDemand(args), convertOnDemandUsers)
 
-    val Array(host, username, password) = args
+  private def fetchList(args: (String, String, String), convertUsers: (String, String, String, Traversable[String]) => Unit) {
+    println("# Generating list of authors...")
+
+    val (host, username, password) = args
     val authors = collection.mutable.Set[String]()
     val proc = List(
       "svn", "log",
       "--trust-server-cert",
       "--non-interactive",
-      "--username", username,
-      "--password", password,
       "--no-auth-cache",
       "--xml", "-q",
-      "https://" + host + "/svn"
-    ).run(BasicIO.standard(false) withOutput { is =>
+      host)
+      .++ (if (username != null) Seq("--username", username) else Seq())
+      .++ (if (password != null) Seq("--password", password) else Seq())
+      .run(BasicIO.standard(false) withOutput { is =>
       val reader = new XMLEventReader(Source.fromInputStream(is))
       // Add the content of the author elements to the authors set declared above.
       reader.foreach(_ match {
@@ -55,12 +57,38 @@ object Authors {
         case _ => ()
       })
     })
+
     if (proc.exitValue != 0) {
       println("SVN command failed!")
       sys.exit(1)
     }
 
-    val apiRoot = url("https://" + host + "/rest/api/latest") as_! (username, password)
+    convertUsers(host, username, password, authors)
+  }
+
+  private def process(args: Array[String]): (String, String, String) = {
+    args match {
+      case (Array(host, username, password)) => (host, username, password)
+      case (Array(host, username)) => (host, username, readLine("password? "))
+      case (Array(host)) => (host, null, null)
+      case _ =>
+        println("Required: host [username] [password]")
+        sys.exit(1)
+    }
+  }
+
+  private def processOnDemand(args: Array[String]): (String, String, String) = {
+    process(args) match {
+      case ((host, username, password)) => ("https://" + host + "/svn", username, password)
+    }
+  }
+
+  private def convertUsers(host: String, username: String, password: String, authors: Traversable[String]) {
+    authors.map(format("%1$s = %1$s <%1$s@mycompany.com>", _)).foreach(println)
+  }
+
+  private def convertOnDemandUsers(host: String, username: String, password: String, authors: Traversable[String]) {
+    val apiRoot = url(host stripSuffix "/svn" concat "/rest/api/latest") as_! (username, password)
     val groupedAuthors = authors
       .par
       // Convert authors to "svnauthor = Git Author <email@address>" where possible.
@@ -71,4 +99,5 @@ object Authors {
     groupedAuthors._1.toArray.sorted.foreach(println)
     groupedAuthors._2.toArray.sorted.foreach(println)
   }
+
 }
