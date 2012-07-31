a68851d181cd5699bba5fbbd3d8433a1ea90a7dd
Huw Giddens <hgiddens@gmail.com>
2012-07-31 15:17:46 +1000

    Implement ‘--strip-metadata’ option.

--- src/main/scala/Clean.scala
+++ src/main/scala/Clean.scala
@@ -1,5 +1,5 @@
 object Clean extends Command {
-  case class Options(shouldCreate: Boolean, shouldDelete: Boolean)
+  case class Options(shouldCreate: Boolean, shouldDelete: Boolean, stripMetadata: Boolean)
 
   val name = "clean-git"
   override val usage = Some("[--dry-run] [--no-delete] [--strip-metadata] <url>...")
@@ -13,7 +13,7 @@ object Clean extends Command {
   def apply(options: Array[String], arguments: Array[String]) = {
     val svnRoots = Set("branches", "tags").map { r => (r, arguments.map { u => { u stripSuffix "/"} + '/' + r})}.toMap
     val dryRun = options.contains("--dry-run")
-    implicit val opts = Options(!dryRun, !(dryRun || options.contains("--no-delete")))
+    implicit val opts = Options(!dryRun, !(dryRun || options.contains("--no-delete")), options.contains("--strip-metadata"))
 
     Tags.annotate()
     Branches.createLocal()
@@ -21,7 +21,16 @@ object Clean extends Command {
     Branches.checkObsolete(svnRoots("branches"))
     Tags.fixNames()
     Branches.fixNames()
+    stripMetadata()
 
     false
+  }
+
+  def stripMetadata()(implicit options: Clean.Options) {
+    import sys.process._
+    if (options.shouldDelete && options.stripMetadata) {
+      println("# removing Subversion metadata from Git commit messages")
+      Seq("git", "filter-branch", "--msg-filter", "sed -e '/^git-svn-id:/d'") !
+    }
   }
 }
