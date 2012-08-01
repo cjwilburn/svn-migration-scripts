eee913fef9e260811fcfba930403da23a98379e9
Huw Giddens <hgiddens@gmail.com>
2012-08-01 16:43:32 +1000

    Simple implementation.

--- src/main/scala/MountDiskImage.scala
+++ src/main/scala/MountDiskImage.scala
@@ -1,10 +1,35 @@
 object MountDiskImage extends Command {
   val name = "mount-disk-image"
+  override val usage = Some("<size-in-gb> <disk-image-file> <mount-point>")
   val help = "Mount a case-sensitive disk image at some path."
   override val available = sys.props("os.name") == "Mac OS X"
 
-  def parse(arguments: Array[String]) = Right(Array(), Array())
+  def parse(arguments: Array[String]) =
+    arguments match {
+      case Array(size, diskImageFile, mountPoint) =>
+        try {
+          val parsedSize = size.toInt
+          if (parsedSize < 0)
+            Left("Cowardly refusing to create a disk image with negative size.")
+          else if (parsedSize == 0)
+            Left("Cowardly refusing to create a disk image with zero size.")
+          else if (parsedSize > 100)
+            Left("Cowardly refusing to create a disk image larger than 100GB.")
+          else
+            Right(Array(), arguments)
+        } catch {
+          case ex: NumberFormatException =>
+            Left("Invalid size argument '%s'.".format(size))
+        }
+      case _ => Left("Invalid arguments")
+    }
+
   def apply(options: Array[String], arguments: Array[String]) = {
-    false
+    import sys.process._
+    val size = arguments(0) + "g"
+    val imagePath = arguments(1) + ".sparseimage" // Append suffix to stop wrong image type being created
+    val mountPath = arguments(2)
+    (Seq("hdiutil", "create", "-size", size, imagePath, "-type", "SPARSE", "-fs", "HFS+", "-fsargs", "-s") #&&
+      Seq("hdiutil", "attach", imagePath, "-mountpoint", mountPath)).! != 0
   }
 }
