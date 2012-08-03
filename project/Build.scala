6f0ff1949159b81d3e18cc5943419eff22253195
Stefan Saasen <ssaasen@atlassian.com>
2012-08-03 19:45:18 +1000

    Fix ProGuard config to retain commons logging classes required for logging to work

--- project/Build.scala
+++ project/Build.scala
@@ -5,13 +5,23 @@ object SvnMigrationScripts extends Build {
 
   lazy val proguard = proguardSettings ++ Seq(
     proguardOptions := Seq(keepMain("com.atlassian.svn2git.Main"),
+      "-dontskipnonpubliclibraryclassmembers",
+      "-dontskipnonpubliclibraryclasses",
       "-keepattributes *Annotation*",
       "-keep public class com.atlassian.** { public protected *; }",
+      "-keep class org.apache.commons.logging.** { public protected *; }",
       "-keep class * implements org.xml.sax.EntityResolver",
+      "-keep class scala.Function*",
+      "-keep class dispatch.**",
+      "-keep class net.liftweb.json.**",
+      "-keep class org.slf4j.**",
       "-keepclassmembers class * { ** MODULE$; }",
       "-keepnames public class com.atlassian.** ",
       "-keepdirectories",
-      "-dontnote"
+      "-dontobfuscate",
+      "-dontoptimize",
+      "-dontnote",
+      "-verbose"
     )
   )
 
