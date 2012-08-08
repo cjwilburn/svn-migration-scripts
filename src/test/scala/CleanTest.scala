20d5286dcb0cae306b104356837d019a3b267679
Charles O'Farrell <cofarrell@atlassian.com>
2012-08-08 14:43:31 +1000

    SVNGIT-222 Extract common writeSVNRemoteConfig for CleanTest

--- src/test/scala/CleanTest.scala
+++ src/test/scala/CleanTest.scala
@@ -6,18 +6,21 @@ import java.io.File
 
 class CleanTest extends mutable.Specification {
 
+  def writeSVNRemoteConfig(dir: File, extra: String = "") = {
+    FileUtils.writeStringToFile(new File(dir, ".git/config"),
+      """
+        |[svn-remote "svn"]
+        |	url = https://a/b
+        |	fetch = trunk:refs/remotes/trunk
+        |	branches = branches/*:refs/remotes/*
+        |	tags = tags/*:refs/remotes/tags/*
+      """.stripMargin + "\n" + extra)
+  }
+
   "test getSVNRoot" >> {
     Verify.withTempGitDir {
       dir =>
-        FileUtils.writeStringToFile(new File(dir, ".git/config"),
-          """
-            |[svn-remote "svn"]
-            |	url = https://a/b
-            |	fetch = trunk:refs/remotes/trunk
-            |	branches = branches/*:refs/remotes/*
-            |	branches = branches2/*/3:refs/remotes/*
-            |	tags = tags/*:refs/remotes/tags/*
-          """.stripMargin)
+        writeSVNRemoteConfig(dir, "branches = branches2/*/3:refs/remotes/*")
         val (branches, tags) = Clean.getSVNRoots(new Cmd(dir))
         branches must equalTo(Array("https://a/b/branches/", "https://a/b/branches2/*/3"))
         tags must equalTo(Array("https://a/b/tags/"))
